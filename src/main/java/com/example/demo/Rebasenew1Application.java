f27f0ceae2eeb2721563fa1737f810ed3dccfbaf
11hussainE <khajahamid786@gmail.com>
2023-01-17 10:46:41 +0530

    first commit

---
+++ src/main/java/com/example/demo/Rebasenew1Application.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rebasenew1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rebasenew1Application.class, args);
+		System.out.println("practise on rebase");
+		// #13 Git Tutorial | Rebase - YouTube
+		
+	}
+
+}
